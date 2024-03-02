ade60626339cfcc8ea686fc46e86b5d853078f1a
Meera <mnambiar@udel.edu>
2024-03-02 13:45:08 -0500

    created background with walls (no collision)

--- minigame/config.py
+++ minigame/config.py
@@ -3,8 +3,29 @@ WIN_HEIGHT = 480
 TILESIZE = 32
 FPS = 60
 
-PLAYER_LAYER = 1
+PLAYER_LAYER = 2
+BLOCK_LAYER = 1
+
 PLAYER_SPEED = 3
 
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+
+tilemap = [
+    "BBBBBBBBBBBBBBBBBBBB",
+    "B..........B.......B",
+    "B.....B....B.......B",
+    "BBBBBBB....B.......B",
+    "B...............B..B",
+    "B...............B..B",
+    "B.....P.........B..B",
+    "B...............B..B",
+    "B......BBBBBBBBBB..B",
+    "B...........B......B",
+    "B...........B......B",
+    "B.....BBBBBBB......B",
+    "B.....B............B",
+    "B..................B",
+    "BBBBBBBBBBBBBBBBBBBB",
+]
