160bd47d9a46a36e6f00d47674f3497e03e92dd5
Meera <mnambiar@udel.edu>
2024-03-02 23:26:39 -0500

    implemented cleaning

--- minigame/minigame1.py
+++ minigame/minigame1.py
@@ -13,6 +13,7 @@ class Game:
 
         self.character_spritesheet = Spritesheet("minigame/images/character.png")
         self.terrain_spritesheet = Spritesheet("minigame/images/terrain.png")
+        self.clean_spritesheet = Spritesheet("minigame/images/clean.png")
         self.intro_background = pygame.image.load("minigame/images/introbackground.png")
         self.go_background = pygame.image.load("minigame/images/gameover.png")
 
@@ -23,7 +24,7 @@ class Game:
                 if column == "B":
                     Block(self, j, i)
                 if column == "P":
-                    Player(self, j, i)
+                    self.player = Player(self, j, i)
                 if column == "X":
                     BananaPeel(self, j, i)
                 
@@ -48,6 +49,17 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.player.facing == "up":
+                        Clean(self, self.player.rect.x+10, self.player.rect.y-7)
+                    if self.player.facing == "down":
+                        Clean(self, self.player.rect.x+10, self.player.rect.y+7)
+                    if self.player.facing == "left":
+                        Clean(self, self.player.rect.x-7, self.player.rect.y)
+                    if self.player.facing == "right":
+                        Clean(self, self.player.rect.x+7, self.player.rect.y-10)
 
     
     def update(self):
